118479d41cd68c25adc84ce7630fcc2ce30a5406
Miguel Dias <miguelviniciusbarbosadias@gmail.com>
2024-09-02 09:56:21 -0300

    gf

--- controller/EventogayController.php
+++ controller/EventogayController.php
@@ -7,8 +7,8 @@ private $eventogayModel;
         $this->eventogayModel = new EventogayModel($pdo);
     }
 
-    public function cadastrar($rua,$bairro,$numero,$cep,$cidade,$estado,$pais){
-        $this->eventogayModel->criarLocalidade($rua,$bairro,$numero,$cep,$cidade,$estado,$pais);
+    public function cadastrar($id, $nome, $datagay, $idade, $acompanhado, $autorizado){
+        $this->eventogayModel->cadastrar($id, $nome, $datagay, $idade, $acompanhado, $autorizado);
     }
 
     public function listarEventogay(){
