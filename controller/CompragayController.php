ac5eee08100f0d0527741a8350fad0e9f0abc160
Miguel Dias <miguelviniciusbarbosadias@gmail.com>
2024-09-02 10:34:13 -0300

    vendagay mais compra

---
+++ controller/CompragayController.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'C:/aluno2/xampp/htdocs/exercicios/model/CompragayModel.php';
+class CompragayController{
+private $compragayModel;
+
+    public function __construct($pdo){
+        $this->compragayModel = new CompragayModel($pdo);
+    }
+
+    public function estoque($id , $nome, $datacompra, $nomeproduto, $quantidade, $vendarealizada){
+        $this->CompragayModel->estoque($id , $nome, $datacompra, $nomeproduto, $quantidade, $vendarealizada);
+    }
+
+    public function listarCompragay(){
+        return $this->CompragayModel->listarCompragay();
+    }
+
+    public function exibirlistarComprargay(){
+        $vendagays = $this->COmpragayModel->listarCompragay();
+        include 'view/vendagay/listar.php';
+    }
+
+} 
--- view/vendagay/estoque.php
+++ view/vendagay/estoque.php
@@ -1,11 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cadastro</title>
+</head>
+<body>
+   <h1>Cadastrar Vendagay</h1>
+   <form method="post">
+    <input type="text" name="codigo" placeholder="Código">
+    <input type="text" name="nome" placeholder="Nome do produto">
+    <input type="text" name="quantidade" placeholder="Quantidade disponivel">
+    <button type="submit">Cadastrar no estoque</button>
+   </form> 
+</body>
+</html>
+
 <?php
 
-foreach($vendagays as $vendagay){
-    echo $vendagay["codigo"] . "<br>";
-    echo $vendagay["nome"] . "<br>";
-    echo $vendagay["quantidade"] . "<br>";
-}
-?>
-<button><a href="view\vendagay\estoque">cadastar estoque</a></button>
+require_once 'C:/aluno2/xampp/htdocs/exercicios/config.php';
+require_once 'C:/aluno2/xampp/htdocs/exercicios/controller/VendagayController.php';
 
-<br><br>
+if(isset($_POST["codigo"]) &&
+ isset($_POST["nome"]) &&
+ isset($_POST["quantidade"])){
+    $VendagayController = new VendagayController($pdo);
+
+    $VendagayController->estoque($_POST["codigo"], $_POST["nome"], $_POST["quantidade"]);
+
+    header("Location: index.php");
+ }
+
+ ?>
